56161dca5c494336a3b9a1382517a237d28ebdf4
Sakthibats <baskaran.sakthivel@gmail.com>
2022-12-09 01:54:07 +0800

    added login authentication

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { useLocation } from "react-router-dom";
+import { useAuth } from '../context/AuthContext';
+import Headercontent from './Headercontent';
+
 
 function Header() {
   const location = useLocation();
-
+  const {currentUser} = useAuth()
   //destructuring pathname from location
   const { pathname } = location;
   const splitLocation = pathname.split("/");
@@ -11,12 +14,8 @@ function Header() {
   return (
     <nav className="navbar navbar-dark align-items-end navbarbg rounded-3 mb-3 mt-2" style={{"height":"10vh"}}>
       <div className="container-md">
-        <a className="navbar-brand fs-2" href="/">Stock News Tracking</a>
-        <ul className="nav justify-content-end">
-          <li className="nav-item">
-            <a className={splitLocation[1] === "" ? "nav-link active" : "nav-link"} href="/search">Searches</a>
-          </li>
-        </ul>
+        <a className="navbar-brand fs-2" href="/">Stock Screener</a>
+        {currentUser&& <Headercontent splitLocation={splitLocation} />}
       </div>
     </nav>
   )
