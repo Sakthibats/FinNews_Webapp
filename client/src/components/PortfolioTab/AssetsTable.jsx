7d9952a90a24b862c4ca0f7d6b381e7d64d6bb69
“sakthibats” <baskaran.sakthivel@gmail.com>
2024-12-15 20:18:08 +0800

    added table

--- client/src/components/PortfolioTab/AssetsTable.jsx
+++ client/src/components/PortfolioTab/AssetsTable.jsx
@@ -11,30 +11,56 @@ function AssetsTable() {
 
   return (
       <>
-          <div className='px-4 has-text-left'>
+          <div className='px-4 has-text-left my-5'>
               <h1>Assets</h1>
           </div>
-          <table>
+          <table className='table is-narrow is-hoverable is-fullwidth rounded'>
               <thead>
                   <tr>
                       <th>Efficient Frontier Assets</th>
-                      <th>Allocation</th>
-                      <th>Min. Weight</th>
-                      <th>Max. Weight</th>
+                      <th>Allocation %</th>
+                      <th>Min. Weight %</th>
+                      <th>Max. Weight %</th>
                   </tr>
               </thead>
               <tbody>
                   {assets.map((asset) => (
                       <tr>
                           <td>
-                              <div>
-                                  <input class="input" type="email" placeholder="Email" />
+                              <div class="control has-icons-left has-icons-right m-2">
+                                  <input class="input is-small" type="email" placeholder="Ticker,ETFs" />
                                   <span class="icon is-small is-left">
-                                      <i class="fas fa-envelope"></i>
+                                      <i class="fas fa-home"></i>
+                                  </span>
+                              </div>
+                          </td>
+                          <td>
+                              <div class="control has-icons-left has-icons-right m-2">
+                                  <input class="input is-small" type="email" placeholder="" />
+                                  <span class="icon is-small is-left">
+                                      <i class="fas fa-calculator"></i>
+                                  </span>
+                              </div>
+                          </td>
+                          <td>
+                              <div class="control has-icons-left has-icons-right m-2">
+                                  <input class="input is-small" type="email" placeholder="sdf" />
+                                  <span class="icon is-small is-left">
+                                      <i class="fas fa-home"></i>
+                                  </span>
+                              </div>
+                          </td>
+                          <td>
+                              <div class="control has-icons-left has-icons-right m-2">
+                                  <input class="input is-small" type="email" placeholder="dfg" />
+                                  <span class="icon is-small is-left">
+                                      <i class="fas fa-home"></i>
                                   </span>
                               </div>
                           </td>
                       </tr>
+                      
+                      
                   ))}
               </tbody>
           </table>
